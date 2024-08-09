9727e6ebec8665782d42038730ac7285ae96679c
J3-Aquacool <jagdish.java2@gmail.com>
2024-08-09 21:36:41 +0530

    Added first commit

---
+++ EcomApp/src/main/java/com/app/EcomApp/App.java
@@ -0,0 +1,15 @@
+package com.app.EcomApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Maven--->Github--->Jenkins----Docker" );
+     
+        System.out.println( "Success" );
+    }
+}
