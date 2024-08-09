0d078e17d5690b394bc051027a03972d16403a61
J3-Aquacool <jagdish.java2@gmail.com>
2024-08-09 23:13:11 +0530

    new update

--- EcomApp/src/main/java/com/app/EcomApp/App.java
+++ EcomApp/src/main/java/com/app/EcomApp/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Maven--->Github--->Jenkins----Docker" );
      
-        System.out.println( "Succes nnn nnnnmmmmmmmns" );
+        System.out.println( "Succemmms nnn nnnnmmmmmmmns" );
     }
 }
